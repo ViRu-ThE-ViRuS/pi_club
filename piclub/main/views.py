d62204a60448e2d6964df435c1d966f0a199329b
Viraat_Chandra <viraat.chandra@hotmail.com>
2017-04-10 17:10:53 +0530

    completed homepage started loginpage

--- piclub/main/views.py
+++ piclub/main/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/home.html')
+
+def login(request):
+    return render(request, 'main/login.html')
