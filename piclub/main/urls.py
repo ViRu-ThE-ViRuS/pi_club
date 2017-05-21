29561d171e25822089f9ca4ae5c910271825fd0b
Viraat_Chandra <viraat.chandra@hotmail.com>
2017-05-21 17:36:30 +0530

    finishing

--- piclub/main/urls.py
+++ piclub/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^home', views.index, name="index"),
     url(r'^main', views.index, name="index"),
     url(r'^login', views.login, name="login"),
     url(r'^sign_up', views.sign_up, name="sign_up"),
